48a0f1e0b1512b50817bad25eb765a43dcb088ee
Quinn Khuc <qtk23@drexel.edu>
2023-01-10 13:13:06 -0500

    Add detail page

---
+++ src/routes/detail/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+ 
+/** @type {import('./$types').PageLoad} */
+export function load = () => {
+  if (params.slug === 'UXID') {
+    return {
+      title: 'Hello world!',
+      content: 'Welcome to our blog. Lorem ipsum dolor sit amet...'
+    };
+  }
+ 
+  throw error(404, 'Not found');
+}
