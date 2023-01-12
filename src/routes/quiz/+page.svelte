8fdb548b43f226a1f53f26dde86863363cb588b2
Sylar Li | Zhengtian Li <sylar@whatscookin.com>
2023-01-11 22:48:53 -0500

    Selected Tags

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,14 +1,37 @@
 <script>
-    let tags = ["Dance", "Engineering", "Architecture", "Filmmaking", "Research", "Storytelling", "History of Art"]
+	let tags = [
+		'Dance',
+		'Engineering',
+		'Architecture',
+		'Filmmaking',
+		'Research',
+		'Storytelling',
+		'History of Art'
+	];
+
+	let selected_tags = ['Storytelling', 'History of Art'];
+
+	import finger_click from '$lib/images/cross.svg';
 </script>
 
-<h1 class="p-8 m-2">Pike Your Interests</h1>
-<div class="p-8 m-2 border-blue-600 border-2 rounded-2xl">
-    <h2>Interests Picked</h2>
-</div>
-<div>
-    {#each tags as tag}
-        <button class="border-blue-600 border-2 rounded-full py-1 px-3 m-1">{tag}</button>
-    {/each}
-</div>
-<a href="/result" class="bg-gray-200 float-right rounded-full py-1 px-3 m-1">View Result</a>
+<main class="p-5">
+	<div>
+		<div>
+			<h1 class="text-2xl font-bold">Pick Your Interests</h1>
+			<p class="font-light text-base">Choose as many tags as you like!</p>
+		</div>
+		<button>Next</button>
+	</div>
+
+
+
+	<div>
+		{#each tags as tag}
+			<button class="tags border-blue-600 border rounded-full py-1 px-3 m-1">{tag}</button>
+		{/each}
+	</div>
+	<a href="/result" class="bg-gray-200 float-right rounded-full py-1 px-3 m-1">View Result</a>
+</main>
+
+<style>
+</style>
