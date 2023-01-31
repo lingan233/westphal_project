8a43e01a1df456504bc9bf2e8c15eab45710a80e
Quinn Khuc <qtk23@drexel.edu>
2023-01-31 18:17:53 -0500

    Update idle page style

--- src/lib/components/BackButton.svelte
+++ src/lib/components/BackButton.svelte
@@ -1,10 +1,10 @@
 <script>
-	import BackArrow from '$lib/icon/BackArrow.svelte';
+	import back_arrow from '$lib/icon/BackArrow.svg';
 </script>
 
 <button
 	class="flex items-center bg-drexel-yellow py-1 pr-5 pl-1 text-xl font-[600] text-drexel-dark-blue"
 >
-	<BackArrow />
+	<img src={back_arrow} alt="Back Arrow" />
 	Back
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
-	import finger_click from '$lib/images/finger.png';
+	import finger_click from '$lib/icon/FingerClick.svg';
 </script>
 
 <a href="/quiz">
-	<img src="https://via.placeholder.com/150" alt="" class="min-h-screen" />
+	<div style="background-image: url('https://res.cloudinary.com/drljdv2k2/image/upload/v1674572664/Westphal%20Touchscreen/img_ukrztb.png')" class=" h-[100vh] bg-center">
+		<div
+			class="absolute top-[60%] right-0 flex bg-drexel-dark-blue py-2 px-4 text-white w-[85%] justify-around items-center"
+		>
+			<h1>Tap to Explore Westphal Majors</h1>
+			<img src={finger_click} alt="Finger Click Icon"/>
+		</div>
+	</div>
 </a>
-<div
-	class="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 bg-drexel-dark-blue p-2 text-white "
->
-	<h1>Tap to Explore Westphal Majors</h1>
-	<img src={finger_click} alt="abc" />
-</div>
+
