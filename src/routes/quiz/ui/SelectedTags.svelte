0b715d41081f9e9f9e6968fbcbd7e83c22c04ef3
lingan233 <71675451+lingan233@users.noreply.github.com>
2023-02-03 20:14:53 -0500

    added the images to the result page and added the selected tags and the search again on the result page and also changed the border of intersts picked text on all the pages

--- src/routes/quiz/ui/SelectedTags.svelte
+++ src/routes/quiz/ui/SelectedTags.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <div class="relative h-32 sm:h-full rounded-3xl border-2 border-drexel-light-blue">
-	<h2 class="absolute top-0 left-4 -translate-y-1/2 bg-white p-1 px-2 text-drexel-dark-blue">
+	<h2 class="absolute top-0 left-4 -translate-y-1/2 bg-white p-1 px-2 text-drexel-dark-blue rounded-full">
 		Interests Picked
 	</h2>
 	<div class="flex max-h-full flex-wrap gap-2 overflow-scroll px-4 py-2 pt-4 align-top">
--- src/routes/result/+page.svelte
+++ src/routes/result/+page.svelte
@@ -2,7 +2,6 @@
     import majors from '../../lib/data/data.json';
 	import { onMount } from "svelte";
 	let tags = [];
-	let matchedMajors = [];
   	let currentMajor;
 	onMount(() => {
 		let urlParams = new URLSearchParams(window.location.search);
@@ -17,9 +16,9 @@
 
 	let allMajors = []
 	onMount(async () => {
-    const res = await Promise.resolve({ json: () => [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] });
-    allMajors = await res.json();
-  });
+		const res = await Promise.resolve({ json: () => [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] });
+		allMajors = await res.json();
+	});
   
     function sortMajors(majors) {
       return majors.sort((a, b) => {
@@ -41,7 +40,22 @@
     <div class="m-2 text-2xl font-bold text-drexel-dark-blue">
         <h1>Recommend For You</h1>
     </div>
-    <div class="grid grid-cols-2 font-semibold">
+	<div class="relative h-32 sm:h-full rounded-3xl border-2 border-drexel-light-blue my-4">
+		<h2 class="absolute top-0 left-4 -translate-y-1/2 bg-white p-1 px-2 text-drexel-dark-blue rounded-full">
+			Interests Picked
+		</h2>
+		<div class="flex max-h-full flex-wrap gap-2 overflow-scroll px-4 py-2 pt-4 align-top">
+			{#each tags as tag}
+				<button class="tags flex h-fit items-center justify-center gap-1 rounded-full border border-drexel-light-blue py-0.5 px-0.5 leading-4">
+					<p class="pl-1 text-drexel-dark-blue">{tag}</p>
+				</button>
+			{/each}
+		</div>
+		<a href="/quiz" class="absolute bottom-0 right-4 translate-y-2/3 text-white bg-drexel-light-blue rounded-full py-1 px-4 m-1 text-sm border-4 border-white">
+			Search Again
+		</a>
+	</div>
+    <div class="grid grid-cols-2 font-semibold py-4">
 		{#each getMajors().slice(0, currentMajor) as major, i}
 			<a href="/detail/{major.id}">
 				<div class="m-2 relative rounded-lg overflow-hidden text-white">
@@ -50,7 +64,7 @@
 					{:else}
 						<div class="py-1 px-4 m-1 text-xs absolute top-0 rounded-full outline bg-white outline-drexel-light-blue text-drexel-light-blue">No.{i+1}</div>
 					{/if}
-					<img src="https://via.placeholder.com/200x250" alt="Avatar" class="object-cover w-full h-full" />
+					<img src={major.content.cover_img} alt="Avatar" class="object-cover w-full h-48" />
 					<div class="absolute w-full py-2.5 bottom-0 inset-x-0 bg-gradient-to-b from-transparent to-black/50 leading-4 p-2">{major.content.name}</div>
 				</div>
 			</a>
