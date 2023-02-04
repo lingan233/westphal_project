d58dd9b8eae17eb41e5f5f8a45a4a709f7c36a98
Quinn Khuc <qtk23@drexel.edu>
2023-02-03 19:34:21 -0500

    Update database

--- src/lib/components/SwipeUp.svelte
+++ src/lib/components/SwipeUp.svelte
@@ -9,4 +9,4 @@
 	<path d="M0.317383 1L63.1729 18.3672L127.682 1" stroke="white" />
 	<path d="M0.317383 9.68359L63.1729 27.0508L127.682 9.68359" stroke="white" />
 </svg>
-<p class="select-none text-center text-white">Swipe up</p>
+<!-- <p class="select-none text-center text-white">Swipe up</p> -->
